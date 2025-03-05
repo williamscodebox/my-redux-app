4f55e2995e79aad62eccb5f4e8813714e03013d3
williamscodebox <138732551+williamscodebox@users.noreply.github.com>
2025-03-05 13:58:52 -0500

    updated usersSlice postsSlice AddPostForm main

--- src/main.jsx
+++ src/main.jsx
@@ -4,6 +4,9 @@ import "./index.css";
 import App from "./App.jsx";
 import { store } from "./app/store.js";
 import { Provider } from "react-redux";
+import { fetchUsers } from "./app/features/users/usersSlice.js";
+
+store.dispatch(fetchUsers());
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>

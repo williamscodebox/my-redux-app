02b135c53467c1c2be71eda6978a2171eab7de7a
williamscodebox <138732551+williamscodebox@users.noreply.github.com>
2025-03-05 13:59:23 -0500

    updated

--- src/app/features/post/postsSlice.js
+++ src/app/features/post/postsSlice.js
@@ -44,6 +44,18 @@ export const fetchPosts = createAsyncThunk("posts/fetchPosts", async () => {
   return response.data;
 });
 
+export const addNewPost = createAsyncThunk(
+  "posts/addNewPost",
+  async (initialPost) => {
+    try {
+      const response = await axios.post(POSTS_URL, initialPost);
+      return response.data;
+    } catch (err) {
+      return err.message;
+    }
+  }
+);
+
 const postSlice = createSlice({
   name: "posts",
   initialState,
@@ -113,6 +125,19 @@ const postSlice = createSlice({
       .addCase(fetchPosts.rejected, (state, action) => {
         state.status = "failed";
         state.error = action.error.message;
+      })
+      .addCase(addNewPost.fulfilled, (state, action) => {
+        action.payload.userId = Number(action.payload.userId);
+        action.payload.data = new Date().toISOString();
+        action.payload.reactions = {
+          thumbsUp: 0,
+          wow: 0,
+          heart: 0,
+          rocket: 0,
+          coffee: 0,
+        };
+        console.log(action.payload);
+        state.posts.push(action.payload);
       });
   },
 });
--- src/app/features/users/usersSlice.js
+++ src/app/features/users/usersSlice.js
@@ -1,10 +1,23 @@
-import { createSlice } from "@reduxjs/toolkit";
+import { createSlice, createAsyncThunk } from "@reduxjs/toolkit";
+import axios from "axios";
+
+const USERS_URL = "https://jsonplaceholder.typicode.com/users";
 
 const initialState = [
+  /*
   { id: "0", name: "Dude Lebowski" },
   { id: "1", name: "Neil Young" },
-  { id: "2", name: "Dave Gray" },
+  { id: "2", name: "Dave Gray" }, */
 ];
+
+export const fetchUsers = createAsyncThunk("users/fetchUsers", async () => {
+  try {
+    const response = await axios.get(USERS_URL);
+    return [...response.data];
+  } catch (err) {
+    return err.message;
+  }
+});
 
 const usersSlice = createSlice({
   name: "users",
@@ -14,6 +27,11 @@ const usersSlice = createSlice({
         userAdded(state, action) {
             state.push(action.payload);
         } */
+  },
+  extraReducers(builder) {
+    builder.addCase(fetchUsers.fulfilled, (state, action) => {
+      return action.payload;
+    });
   },
 });
 

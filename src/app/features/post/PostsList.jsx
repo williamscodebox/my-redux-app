668ebf86b4f7a04f2918a6236bac357d53152d53
williamscodebox <138732551+williamscodebox@users.noreply.github.com>
2025-03-05 00:59:22 -0500

    Added TimeAgo and ReactionButtons

--- src/app/features/post/PostsList.jsx
+++ src/app/features/post/PostsList.jsx
@@ -2,18 +2,26 @@ import React from "react";
 import { useSelector, useDispatch } from "react-redux";
 import { selectAllPosts } from "./postsSlice";
 import PostAuthor from "./PostAuthor";
+import TimeAgo from "./TimeAgo";
+import ReactionButtons from "./ReactionButtons";
 
 function PostsList() {
   const posts = useSelector(selectAllPosts);
 
-  const renderedPosts = posts.map((post) => (
-    <div>
+  const orderedPosts = posts
+    .slice()
+    .sort((a, b) => b.date.localeCompare(a.date));
+
+  const renderedPosts = orderedPosts.map((post) => (
+    <div key={post.id}>
       <article className="posts" key={post.id}>
         <h3>{post.title}</h3>
         <p>{post.content.substring(0, 100)}</p>
         <p className="test">
           <PostAuthor userId={post.userId} />
+          <TimeAgo timestamp={post.date} />
         </p>
+        <ReactionButtons post={post} />
       </article>
     </div>
   ));

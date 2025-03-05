8d620c1c39440bbee704e2d372cb983151003d61
williamscodebox <138732551+williamscodebox@users.noreply.github.com>
2025-03-04 19:21:49 -0500

    Added usersSlice

--- src/app/features/post/AddPostForm.jsx
+++ src/app/features/post/AddPostForm.jsx
@@ -1,23 +1,37 @@
 import React, { useState } from "react";
-import { useDispatch } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
 
 import { postAdded } from "./postsSlice";
+import { selectAllUsers } from "../users/usersSlice";
 
 function AddPostForm() {
   const dispatch = useDispatch();
 
   const [title, setTitle] = useState("");
   const [content, setContent] = useState("");
+  const [userId, setUserId] = useState("");
+
+  const users = useSelector(selectAllUsers);
 
   const onTitleChanged = (e) => setTitle(e.target.value);
   const onContentChanged = (e) => setContent(e.target.value);
+  const onAuthorChanged = (e) => setUserId(e.target.value);
+
   const onSavedPostClicked = () => {
     if (title && content) {
-      dispatch(postAdded(title, content));
+      dispatch(postAdded(title, content, userId));
       setTitle("");
       setContent("");
     }
   };
+
+  const canSave = Boolean(title) && Boolean(content) && Boolean(userId);
+
+  const usersOptions = users.map((user) => (
+    <option key={user.id} value={user.id}>
+      {user.name}
+    </option>
+  ));
 
   return (
     <section>
@@ -31,6 +45,11 @@ function AddPostForm() {
           value={title}
           onChange={onTitleChanged}
         />
+        <label htmlFor="postAuthor">Author:</label>
+        <select id="postAuthor" value={userId} onChange={onAuthorChanged}>
+          <option value=""></option>
+          {usersOptions}
+        </select>
         <label htmlFor="postContent">Content:</label>
         <textarea
           id="postContent"
@@ -38,7 +57,7 @@ function AddPostForm() {
           value={content}
           onChange={onContentChanged}
         />
-        <button type="button" onClick={onSavedPostClicked}>
+        <button type="button" onClick={onSavedPostClicked} disabled={!canSave}>
           Save Post
         </button>
       </form>
--- src/app/store.js
+++ src/app/store.js
@@ -1,10 +1,12 @@
 import { configureStore } from "@reduxjs/toolkit";
 import counterReducer from "./features/counter/counterSlice.js";
 import postReducer from "./features/post/postsSlice";
+import usersReducer from "./features/users/usersSlice";
 
 export const store = configureStore({
   reducer: {
     counter: counterReducer,
     posts: postReducer,
+    users: usersReducer,
   },
 });
